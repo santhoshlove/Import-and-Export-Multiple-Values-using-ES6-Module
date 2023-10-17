652f4eb5ac90cf46df3a077ea0a2cfbd665a3594
santhoshlove <sg.samthosh.007@gmail.com>
2023-10-17 17:16:37 +0000

    Import and Export Multiple Values using ES6 Module

---
+++ importMultipleValues.mjs
@@ -0,0 +1,7 @@
+import { myArray } from "./exportMultipleValues.mjs";
+import { bulb } from "./exportMultipleValues.mjs";
+import { multi } from "./exportMultipleValues.mjs";
+
+console.log(myArray);
+console.log(bulb);
+console.log(multi(4));
